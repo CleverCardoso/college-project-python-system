2a400840a0bf2a974168d18566cde35e1c2bc6d9
CleverCardoso <clebercardoso.art@gmail.com>
2025-08-04 12:52:19 -0300

    Initial commit

---
+++ Sistema-cafeteria.py
@@ -0,0 +1,8 @@
+while True:
+    print("==== COFFEE SHOPS TIA ROSA ====")
+    print("1. Cadastrar produto")
+    print("2. Cadastrar cliente")
+    print("3. Fazer pedido")
+    print("4. Ver cardápio")
+    print("5. Sair")
+    opcao = input("Escolha uma opção: ")
