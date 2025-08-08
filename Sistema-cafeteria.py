11f80dc2986fdcaf8fbe87264d76357377c92a57
CleverCardoso <clebercardoso.art@gmail.com>
2025-08-08 01:02:28 -0300

    Reestruturação de Código

--- Sistema-cafeteria.py
+++ Sistema-cafeteria.py
@@ -2,9 +2,6 @@ from tabulate import tabulate
 import json
 import re
 
-Produtos = list()
-Clientes = list()
-
 def linha(tam=42):
     return '-' *tam
 
@@ -15,10 +12,10 @@ def cabeçalho(txt):
     
 class produtos:
     def __init__(self):
-        pass
+        self.lista_produto = []
     
        
-    def Cadastrar_produto():
+    def Cadastrar_produto(self):
         nome = input("Nome do Produto: ")
         preco = input("Preço do Produto: ")
         ingredientes_input = input("Digite os ingredientes separados por vírgula: ")
@@ -34,15 +31,14 @@ class produtos:
             promocao = False
 
     
-        Produtos.append({"nome": nome, "preco": preco, "ingredientes": ingredientes, "promocao": promocao})
-        print(f'{Produtos}')
+        self.lista_produto.append({"nome": nome, "preco": preco, "ingredientes": ingredientes, "promocao": promocao})
         
-    def Ver_Cardapio():
+    def Ver_produtos(self):
         cabeçalho("Cardapio")
         print("-" * 80)
         print(f"{'Nome':<20} {'Preço':<10} {'Ingredientes':<30} {'Promoção':<10}")
         print("-" * 80)
-        for produto in Produtos:
+        for produto in self.lista_produto:
             nome = produto["nome"]
             preco = f"R${produto['preco']}"
             ingredientes = ", ".join(produto["ingredientes"])
@@ -50,28 +46,15 @@ class produtos:
             print(f"{nome:<20} {preco:<10} {ingredientes:<30} {promocao:<10}")
             print("-" * 80)
     
-    while True:
-        print("==== PRODUTOS ====")
-        print("1. Cadastrar produto")
-        print("2. Ver lista de produtos")
-        print("3. Sair")
-        opcao = input("Escolha uma opção: ")
-        
-        if opcao == "1":
-            Cadastrar_produto()
-        elif opcao == "2":
-            Ver_Cardapio()
-        elif opcao == "3":
-            break
-        else:
-            print("Opção inválida")
+    
+
 
 class Clientes:
     
     def __init__(self):
-        pass
+        self.lista_clientes = []
 
-    def cadastrar_cliente():
+    def cadastrar_cliente(self):
         nome = input("Nome do cliente: ")
         tel = input("Telefone do cliente: ")
 
@@ -83,32 +66,69 @@ class Clientes:
             else:
                 print("Email inválido. Tente novamente.")
 
-        Clientes.append({"nome": nome, "tel": tel, "email": email})
+        self.lista_clientes.append({"nome": nome, "tel": tel, "email": email})
 
 
-    
+def menu_produtos():
+    while True:
+                print("==== PRODUTOS ====")
+                print("1. Cadastrar produto")
+                print("2. Ver lista de produtos")
+                print("3. Sair")
+                opcao = input("Escolha uma opção: ")
+                
+                if opcao == "1":
+                    p.Cadastrar_produto()
+                elif opcao == "2":
+                    p.Ver_Cardapio()
+                elif opcao == "3":
+                    break
+                else:
+                    print("Opção inválida")
+                    
+class pedido:
+    def __init__(self):
+        pass
 
+def Ver_cardapio(self):
+        cabeçalho("Cardapio")
+        print("-" * 80)
+        print(f"{'Nome':<20} {'Preço':<10} {'Ingredientes':<30} {'Promoção':<10}")
+        print("-" * 80)
+        for produto in self.lista_produto:
+            nome = produto["nome"]
+            preco = f"R${produto['preco']}"
+            ingredientes = ", ".join(produto["ingredientes"])
+            promocao = "Sim" if produto["promocao"] else "Não"
+            print(f"{nome:<20} {preco:<10} {ingredientes:<30} {promocao:<10}")
+            print("-" * 80)
+p = produtos()
+c = Clientes()    
+
+def main_menu():       
+    while True:
+        print("==== COFFEE SHOPS TIA ROSA ====")
+        print("1. Cadastrar produto")
+        print("2. Clientes")
+        print("3. Fazer pedido")
+        print("4. Ver cardápio")
+        print("5. Sair")
+        opcao = input("Escolha uma opção: ")
         
-while True:
-    print("==== COFFEE SHOPS TIA ROSA ====")
-    print("1. Cadastrar produto")
-    print("2. Clientes")
-    print("3. Fazer pedido")
-    print("4. Ver cardápio")
-    print("5. Sair")
-    opcao = input("Escolha uma opção: ")
-    
-    if opcao == "1":
-        produtos
-    elif opcao == "2":
-        Clientes
-    elif opcao == "3":
-        pass
-    elif opcao == "4":
-        pass
-    elif opcao == "5":
-        break
-    else:
-        print("Opção inválida")
+        if opcao == "1":
+            menu_produtos()
+        elif opcao == "2":
+            Clientes
+        elif opcao == "3":
+            pass
+        elif opcao == "4":
+            pass
+        elif opcao == "5":
+            break
+        else:
+            print("Opção inválida")
+            
+            
+main_menu()
         
     
